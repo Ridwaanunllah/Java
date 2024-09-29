84594dce9d9a1582b9dbc40bd863bb4304c5f8b3
Ridwaanunllah <mcsalley4@gmail.com>
2024-09-29 12:34:37 +0000

    sample works

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hello world");
+        //Creating variables (lesson 1) Numeric data types
+        byte age = 1;
+        short time = 12;
+        int phone = 10;
+        long ages = 25;
+        double shoe = 1.8;
+        float car = 13f;
+        System.out.print(age + " " + time + " " + phone + " " + ages + " " + shoe + " " + car + " ");
+    }
+}
